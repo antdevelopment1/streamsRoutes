909b6ead037879d5fd0094fd05b4dad8799ed0ce
April Copes <antdevelopment1@gmail.com>
2018-12-17 21:16:35 -0500

    Adds route for pageone and two

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+
+const pageOne = () => {
+    return (
+        <div>
+            Page One
+        </div>
+    )
+}
+
+const pageTwo = () => {
+    return (
+        <div>
+            pageTwo
+            <button>Click Me!</button>
+        </div>
+    )
+}
 
 const App = () => {
     return(
-        <div> hi</div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={pageOne} />
+                    <Route path="/pageTwo" component={pageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
     )
 }
 
